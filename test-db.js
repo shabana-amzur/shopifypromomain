9327fcc7780fa8d80fc5f194228809a338d3801f
shabana-amzur <shabana.sheik@amzur.com>
2025-12-09 16:08:25 +0530

    Update test-db.js to ensure subscribers table is created if missing and test insert operation

--- test-db.js
+++ test-db.js
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 async function testDatabase() {
-  const connectionString = 'postgresql://neondb_owner:npg_AxJDb8ZokH6X@ep-muddy-band-a4c31lkn-pooler.us-east-1.aws.neon.tech/shopifypromoapp?sslmode=require';
+  const connectionString = 'postgresql://neondb_owner:npg_HTY06viblkoX@ep-little-wind-a45qfyi2-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
   
   const client = new Client({
     connectionString: connectionString,
