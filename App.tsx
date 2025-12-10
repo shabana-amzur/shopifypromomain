640793f033327fdbd9f12837a4c4fd0786af07ef
shabana-amzur <shabana.sheik@amzur.com>
2025-12-10 13:36:09 +0530

    Add HeroAlt section to display both hero banners

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 import Hero from './components/Sections/Hero';
+import HeroAlt from './components/Sections/HeroAlt';
 import Features from './components/Sections/Features';
 import FAQ from './components/Sections/FAQ';
 import CTA from './components/Sections/CTA';
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <Header />
       <main className="flex-grow">
         <Hero />
+        <HeroAlt />
         <Features />
         <FAQ />
         <CTA />
