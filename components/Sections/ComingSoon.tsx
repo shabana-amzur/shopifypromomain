70240485647fa6ca1799539398a4ed497d22d354
shabana-amzur <shabana.sheik@amzur.com>
2026-01-12 15:40:55 +0530

    Remove Coming Soon subtitle

--- components/Sections/ComingSoon.tsx
+++ components/Sections/ComingSoon.tsx
@@ -34,9 +34,6 @@ const ComingSoon: React.FC = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center max-w-2xl mx-auto mb-12">
           <h2 className="text-3xl md:text-5xl font-semibold text-white mb-4">Coming Soon....</h2>
-          <p className="text-lg text-slate-200">
-            Unlock even more promotion power
-          </p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
